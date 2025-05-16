a3527c547b1f1c5dec088865e006183999e3e6fc
supraja.s <supraja.s@goavega.com>
2025-05-16 22:54:36 +0530

    feat: bootstrap sanity studio

---
+++ sanity.cli.ts
@@ -0,0 +1,13 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'zzb6y3nc',
+    dataset: 'production'
+  },
+  /**
+   * Enable auto-updates for studios.
+   * Learn more at https://www.sanity.io/docs/cli#auto-updates
+   */
+  autoUpdates: true,
+})
